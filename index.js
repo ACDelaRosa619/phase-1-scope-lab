815cec57cbc92ec4c44dccd56213b8fcbf284486
Adrian <acdelarosa19@gmail.com>
2023-11-16 16:18:22 -0800

    Customers

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function myFunc() {
+    console.log(customerName)
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCusomter = 'Adrian'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCusomter = 'Daniel'
+}
